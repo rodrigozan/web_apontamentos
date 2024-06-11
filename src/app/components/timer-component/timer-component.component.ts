a6d916132b8fdf46740e26e0d411e8f39ce2d365
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-06-11 08:27:58 -0300

    Created timer component

---
+++ src/app/components/timer-component/timer-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timer-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './timer-component.component.html',
+  styleUrl: './timer-component.component.scss'
+})
+export class TimerComponentComponent {
+
+}
